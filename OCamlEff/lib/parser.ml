c691aefcd4fbb2c6049f25acb7814068f404aee2
Alexander Fedkin <st087201@student.spbu.ru>
2021-11-24 19:36:11 +0300

    small fix in cons list parser

--- OCamlEff/lib/parser.ml
+++ OCamlEff/lib/parser.ml
@@ -80,7 +80,7 @@ let lsb = token "["
 let comma = token ","
 let semicol = token ";"
 let between l r p = l *> p <* r
-let parens = between lp rp
+let parens p = between lp rp p
 
 (****************** Tokens ******************)
 
@@ -231,9 +231,12 @@ let pat =
   let plist =
     let plistbr = between lsb rsb (sep_by1 semicol pat <|> return []) >>| plist in
     let plistcons =
+      fix
+      @@ fun plistcons ->
       choice [ between lp rp pat; plistbr; pprimitive ]
       <* tlistcons
       >=> (plistbr
+          <|> parens plistcons
           <|> pwild
           >>| function
           | PList l -> l
@@ -256,19 +259,17 @@ let%test _ = test_pat_suc "[a; f]" @@ PList [ PVar "a"; PVar "f" ]
 let%test _ = test_pat_suc {|"some_string"|} @@ PConst (CString "some_string")
 let%test _ = test_pat_suc "_ :: []" @@ PList [ PWild ]
 let%test _ = test_pat_suc "[[[1]]]" @@ PList [ PList [ PList [ PConst (CInt 1) ] ] ]
-
-let%test _ =
-  test_pat_suc
-    "[[1]; 2 :: []]"
-    (PList [ PList [ PConst (CInt 1) ]; PList [ PConst (CInt 2) ] ])
-;;
-
 let%test _ = test_pat_suc "true :: _" @@ PList [ PConst (CBool true); PWild ]
 let%test _ = test_pat_suc "_, a" @@ PTuple [ PWild; PVar "a" ]
 let%test _ = test_pat_suc "(a), (b)" @@ PTuple [ PVar "a"; PVar "b" ]
 let%test _ = test_pat_suc "(((a), (b)))" @@ PTuple [ PVar "a"; PVar "b" ]
 let%test _ = test_pat_suc "((((a))))" @@ PVar "a"
 
+let%test _ =
+  test_pat_suc
+    "[[1]; 2 :: []]"
+    (PList [ PList [ PConst (CInt 1) ]; PList [ PConst (CInt 2) ] ])
+;;
 let%test _ =
   test_pat_suc "(1, 2) :: []" @@ PList [ PTuple [ PConst (CInt 1); PConst (CInt 2) ] ]
 ;;

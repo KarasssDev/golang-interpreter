1f5c4cfdf3f3182bf01c3ddf05693b47d69ed31f
zishkaz <loktevsergay.2003@gmail.com>
2021-11-29 04:35:56 +0300

    Pushka

--- OCamlEff/lib/eval.ml
+++ OCamlEff/lib/eval.ml
@@ -7,6 +7,7 @@ type exval =
   | StringV of string
   | TupleV of exval list
   | ListV of exval list
+  | FunV of pat * exp * exval Env.t
 
 type env = exval Env.t
 
@@ -16,6 +17,10 @@ let rec match_pat pat var =
   match pat, var with
   | PWild, _ -> []
   | PVar name, v -> [ name, v ]
+  | PCons (pat1, pat2), ListV vars ->
+    (match vars with
+    | [ hd; tl ] -> match_pat pat1 hd @ match_pat pat2 tl
+    | _ -> failwith "Interpretation error: Wrong match.")
   | PTuple pats, TupleV vars when List.length pats = List.length vars ->
     List.fold_left2 (fun binds pat var -> binds @ match_pat pat var) [] pats vars
   | PList pats, TupleV vars when List.length pats = List.length vars ->
@@ -91,6 +96,12 @@ let rec eval_exp env = function
     apply_unary_op op exp_x
   | EList exps -> ListV (List.map (eval_exp env) exps)
   | ETuple exps -> TupleV (List.map (eval_exp env) exps)
+  | ECons (exp1, exp2) ->
+    let exp1_evaled = eval_exp env exp1 in
+    let exp2_evaled = eval_exp env exp2 in
+    (match exp2_evaled with
+    | ListV list -> ListV ([ exp1_evaled ] @ list)
+    | x -> ListV [ exp1_evaled; x ])
   | EIf (exp1, exp2, exp3) ->
     (match eval_exp env exp1 with
     | BoolV true -> eval_exp env exp2
@@ -101,27 +112,35 @@ let rec eval_exp env = function
       List.fold_left
         (fun env binding ->
           match binding with
-          | false, pat, exp ->
+          (* Think of let and let rec *)
+          | _, pat, exp ->
             let evaled = eval_exp env exp in
             let binds = match_pat pat evaled in
-            List.fold_left (fun env (id, v) -> extend id v env) env binds
-          | _ -> failwith "unimpl")
+            List.fold_left (fun env (id, v) -> extend id v env) env binds)
         env
         bindings
     in
     eval_exp gen_env exp1
+  | EFun (pat, exp) -> FunV (pat, exp, env)
+  | EApp (exp1, exp2) ->
+    (match eval_exp env exp1 with
+    | FunV (pat, exp, fenv) ->
+      let binds = match_pat pat (eval_exp env exp2) in
+      let env = List.fold_left (fun env (id, v) -> extend id v env) fenv binds in
+      eval_exp env exp
+    | _ -> failwith "Interpretation error: wrong application")
   | _ -> failwith "Interpretation error: unimpl"
 ;;
 
-let rec eval_dec env = function
+let eval_dec env = function
   | DLet bindings ->
     (match bindings with
-    | false, pat, exp ->
+    (* Think of let and let rec *)
+    | _, pat, exp ->
       let evaled = eval_exp env exp in
       let binds = match_pat pat evaled in
       let env = List.fold_left (fun env (id, v) -> extend id v env) env binds in
-      env
-    | _ -> failwith "Interpretation error: unimpl")
+      env)
   | _ -> failwith "Interpretation error: unimpl"
 ;;
 
@@ -133,118 +152,214 @@ let str_converter = function
 ;;
 
 let exval_to_str = function
-  | Some (IntV x) -> str_converter (IntV x)
-  | Some (BoolV x) -> str_converter (BoolV x)
-  | Some (StringV x) -> str_converter (StringV x)
-  | None -> failwith "Interpretation error: what?"
-  | Some (TupleV x) -> String.concat " " (List.map str_converter x)
-  | Some (ListV x) -> String.concat " " (List.map str_converter x)
+  | IntV x -> str_converter (IntV x)
+  | BoolV x -> str_converter (BoolV x)
+  | StringV x -> str_converter (StringV x)
+  | TupleV x -> String.concat " " (List.map str_converter x)
+  | ListV x -> String.concat " " (List.map str_converter x)
+  | FunV (pat, _, _) ->
+    (match pat with
+    | PVar x -> x
+    | _ -> "error")
 ;;
 
-let eval_test ast num =
-  Printf.printf "Eval test %s\n" num;
-  let evaled = eval_dec Env.empty ast in
-  let env = evaled in
-  IdMap.iter (fun k v -> Printf.printf "%s -> %s\n" k (exval_to_str !v)) env;
-  true
+type decls = decl list
+
+let eval_test decls expected =
+  try
+    let init_env = Env.empty in
+    let env =
+      List.fold_left
+        (fun env decl ->
+          let new_env = eval_dec env decl in
+          new_env)
+        init_env
+        decls
+    in
+    let res =
+      IdMap.fold
+        (fun k v ln ->
+          let new_res = ln ^ Printf.sprintf "%s -> %s " k (exval_to_str !v) in
+          new_res)
+        env
+        ""
+    in
+    if res = expected then true else false
+  with
+  | Tuple_compare ->
+    if expected = "Interpretation error: Cannot compare tuples of different size."
+    then true
+    else false
 ;;
 
 (* Eval test 1 *)
-let%test _ = eval_test (DLet (false, PVar "x", EConst (CInt 1))) "1"
+
+(* 
+  let x = 1
+*)
+let%test _ = eval_test [ DLet (false, PVar "x", EConst (CInt 1)) ] "x -> 1 "
 
 (* Eval test 2 *)
+
+(* 
+  let (x, y) = (1, 2)
+*)
 let%test _ =
   eval_test
-    (DLet
-       (false, PTuple [ PVar "x"; PVar "y" ], ETuple [ EConst (CInt 1); EConst (CInt 2) ]))
-    "2"
+    [ DLet
+        (false, PTuple [ PVar "x"; PVar "y" ], ETuple [ EConst (CInt 1); EConst (CInt 2) ])
+    ]
+    "x -> 1 y -> 2 "
 ;;
 
 (* Eval test 3 *)
+
+(* 
+  let x = 3 < 2
+*)
 let%test _ =
-  eval_test (DLet (false, PVar "x", EOp (Less, EConst (CInt 3), EConst (CInt 2)))) "3"
+  eval_test
+    [ DLet (false, PVar "x", EOp (Less, EConst (CInt 3), EConst (CInt 2))) ]
+    "x -> false "
 ;;
 
 (* Eval test 4 *)
+
+(* 
+  let x = (1, 2) < (1, 2, 3)
+*)
 let%test _ =
-  try
-    eval_test
-      (DLet
-         ( false
-         , PVar "x"
-         , EOp
-             ( Less
-             , ETuple [ EConst (CInt 1); EConst (CInt 2) ]
-             , ETuple [ EConst (CInt 1); EConst (CInt 2); EConst (CInt 3) ] ) ))
-      "4"
-  with
-  | Tuple_compare ->
-    Printf.printf "Interpretation error: Cannot compare tuples of different size.\n";
-    true
+  eval_test
+    [ DLet
+        ( false
+        , PVar "x"
+        , EOp
+            ( Less
+            , ETuple [ EConst (CInt 1); EConst (CInt 2) ]
+            , ETuple [ EConst (CInt 1); EConst (CInt 2); EConst (CInt 3) ] ) )
+    ]
+    "Interpretation error: Cannot compare tuples of different size."
 ;;
 
 (* Eval test 5 *)
+
+(* 
+  let x =
+    let y = 5
+    in y
+*)
 let%test _ =
   eval_test
-    (DLet (false, PVar "x", ELet ([ false, PVar "y", EConst (CInt 5) ], EVar "y")))
-    "5"
+    [ DLet (false, PVar "x", ELet ([ false, PVar "y", EConst (CInt 5) ], EVar "y")) ]
+    "x -> 5 "
 ;;
 
 (* Eval test 6 *)
 
-(* let x =
+(* 
+  let x =
     let y = 5 in
     let z = 10 in
     y + z
-;; *)
+*)
 let%test _ =
   eval_test
-    (DLet
-       ( false
-       , PVar "x"
-       , ELet
-           ( [ false, PVar "y", EConst (CInt 5); false, PVar "z", EConst (CInt 10) ]
-           , EOp (Add, EVar "y", EVar "z") ) ))
-    "6"
+    [ DLet
+        ( false
+        , PVar "x"
+        , ELet
+            ( [ false, PVar "y", EConst (CInt 5); false, PVar "z", EConst (CInt 10) ]
+            , EOp (Add, EVar "y", EVar "z") ) )
+    ]
+    "x -> 15 "
 ;;
 
 (* Eval test 7 *)
 
-(* let x =
+(* 
+  let x =
     let y = 5 in
     let y = 10 in
     y
-;; *)
+*)
 let%test _ =
   eval_test
-    (DLet
-       ( false
-       , PVar "x"
-       , ELet
-           ( [ false, PVar "y", EConst (CInt 5); false, PVar "y", EConst (CInt 10) ]
-           , EVar "y" ) ))
-    "7"
+    [ DLet
+        ( false
+        , PVar "x"
+        , ELet
+            ( [ false, PVar "y", EConst (CInt 5); false, PVar "y", EConst (CInt 10) ]
+            , EVar "y" ) )
+    ]
+    "x -> 10 "
 ;;
 
 (* Eval test 8 *)
 
-(* let x =
+(* 
+  let x =
     let y =
       let y = 10 in
       5
     in
     y
-;; *)
+*)
 let%test _ =
   eval_test
-    (DLet
-       ( false
-       , PVar "x"
-       , ELet
-           ( [ ( false
-               , PVar "y"
-               , ELet ([ false, PVar "y", EConst (CInt 10) ], EConst (CInt 5)) )
-             ]
-           , EVar "y" ) ))
-    "8"
+    [ DLet
+        ( false
+        , PVar "x"
+        , ELet
+            ( [ ( false
+                , PVar "y"
+                , ELet ([ false, PVar "y", EConst (CInt 10) ], EConst (CInt 5)) )
+              ]
+            , EVar "y" ) )
+    ]
+    "x -> 5 "
+;;
+
+(* Eval test 9 *)
+
+(* 
+  let f x y = x + y
+*)
+let%test _ =
+  eval_test
+    [ DLet
+        (false, PVar "f", EFun (PVar "x", EFun (PVar "y", EOp (Add, EVar "x", EVar "y"))))
+    ]
+    "f -> x "
+;;
+
+(* Eval test 10 *)
+
+(* 
+  let f x y = x + y
+  let a = f 1 2 
+*)
+let%test _ =
+  eval_test
+    [ DLet
+        (false, PVar "f", EFun (PVar "x", EFun (PVar "y", EOp (Add, EVar "x", EVar "y"))))
+    ; DLet (false, PVar "a", EApp (EApp (EVar "f", EConst (CInt 1)), EConst (CInt 2)))
+    ]
+    "a -> 3 f -> x "
+;;
+
+(* Eval test 11 *)
+
+(* 
+let f x y = x + y
+let kek = f 1
+let lol = kek 2 
+*)
+let%test _ =
+  eval_test
+    [ DLet
+        (false, PVar "f", EFun (PVar "x", EFun (PVar "y", EOp (Add, EVar "x", EVar "y"))))
+    ; DLet (false, PVar "kek", EApp (EVar "f", EConst (CInt 1)))
+    ; DLet (false, PVar "lol", EApp (EVar "kek", EConst (CInt 2)))
+    ]
+    "f -> x kek -> y lol -> 3 "
 ;;
--- OCamlEff/lib/parser.ml
+++ OCamlEff/lib/parser.ml
@@ -1,5 +1,6 @@
 open Angstrom
 open Ast
+open Typing
 open Format
 
 (****************** Main parser ******************)
@@ -36,7 +37,6 @@ let chainl1 e op =
 ;;
 
 let rec chainr1 e op = e >>= fun a -> op >>= (fun f -> chainr1 e op >>| f a) <|> return a
-let ( >=> ) p1 p2 = many1 p1 >>= fun lhs -> p2 >>= fun rhs -> return @@ lhs @ rhs
 let mklist a = [ a ]
 
 let is_ws = function
@@ -53,6 +53,8 @@ let is_digit = function
   | '0' .. '9' -> true
   | _ -> false
 ;;
+
+let skip = take_while (fun c -> is_ws c || is_eol c)
 
 let is_keyword = function
   | "let"
@@ -120,7 +122,7 @@ let div = token "/" *> return (eop Div)
 let mul = token "*" *> return (eop Mul)
 let less = token "<" *> return (eop Less)
 let _and = token "&&" *> return (eop And)
-let cons = token "::" *> return (fun e1 e2 -> ECons (e1, e2))
+let eopcons = token "::" *> return (fun e1 e2 -> ECons (e1, e2))
 let _or = token "||" *> return (eop Or)
 let eunop o e = EUnOp (o, e)
 let evar id = EVar id
@@ -147,6 +149,7 @@ let pvar id = PVar id
 let pconst c = PConst c
 let plist pl = PList pl
 let ptuple pl = PTuple pl
+let popcons = token "::" *> return (fun p1 p2 -> PCons (p1, p2))
 
 (****************** Decl ctors ******************)
 
@@ -164,11 +167,7 @@ let cmp e1 e2 =
   choice cmps
 ;;
 
-let id =
-  let check_fst = function
-    | 'a' .. 'z' | '_' -> true
-    | _ -> false
-  in
+let id_ check_fst =
   let check_inner c = check_fst c || is_digit c in
   satisfy check_fst
   >>= fun fst ->
@@ -178,9 +177,16 @@ let id =
     check_inner
   >>= function
   | _ as inner ->
-    if is_keyword (Char.escaped fst ^ inner)
+    if is_keyword @@ Char.escaped fst ^ inner
     then fail "Keyword is reserved"
-    else return (Char.escaped fst ^ inner)
+    else return @@ Char.escaped fst ^ inner
+;;
+
+let id =
+  id_
+  @@ function
+  | 'a' .. 'z' | '_' -> true
+  | _ -> false
 ;;
 
 (****************** Const parsing ******************)
@@ -216,42 +222,43 @@ let uns_const = ws *> choice [ cunsint; cbool; cstring ] <* ws
 let pvar = id >>| pvar
 let pwild = twild >>| pwild
 let pconst = const >>| pconst
-let pprimitive = choice [ pconst; pvar; pwild ]
 
-let pat =
-  fix
-  @@ fun pat ->
-  let plist =
-    let plistbr = between lsb rsb (sep_by semicol pat) >>| plist in
-    let plistcons =
-      fix
-      @@ fun plistcons ->
-      choice [ between lp rp pat; plistbr; pprimitive ]
-      <* tlistcons
-      >=> (plistbr
-          <|> parens plistcons
-          <|> pwild
-          >>| function
-          | PList l -> l
-          | _ as rhs -> [ rhs ])
-      >>| plist
-    in
-    plistcons <|> plistbr
+type pdispatch =
+  { tuple : pdispatch -> pat t
+  ; cons : pdispatch -> pat t
+  ; pat : pdispatch -> pat t
+  }
+
+let pcons = return @@ fun p1 p2 -> PCons (p1, p2)
+
+let pack =
+  let pat d = fix @@ fun _self -> ws *> (d.tuple d <|> d.cons d) <* ws in
+  let tuple d =
+    fix
+    @@ fun _self ->
+    ws *> lift2 (fun hd tl -> hd :: tl) (d.cons d) (many1 (comma *> d.cons d))
+    >>| ptuple
+    <* ws
   in
-  let ptuple =
-    let tuplemember = choice [ between lp rp pat; plist; pprimitive ] in
-    tuplemember <* comma <* ws >=> (tuplemember >>| mklist) >>| ptuple
+  let cons d =
+    fix
+    @@ fun _self ->
+    let plist = ws *> (between lsb rsb @@ sep_by semicol @@ d.pat d >>| plist) <* ws in
+    let prim = ws *> choice [ pconst; pvar; pwild; plist; parens @@ d.pat d ] <* ws in
+    ws *> chainr1 prim popcons <* ws
   in
-  ws *> choice [ ptuple; plist; pprimitive; between lp rp pat ] <* ws
+  { tuple; cons; pat }
 ;;
+
+let pat = pack.pat pack
 
 (****************** Expr parsing ******************)
 
-type dispatch =
-  { key : dispatch -> exp t
-  ; tuple : dispatch -> exp t
-  ; exp : dispatch -> exp t
-  ; op : dispatch -> exp t
+type edispatch =
+  { key : edispatch -> exp t
+  ; tuple : edispatch -> exp t
+  ; exp : edispatch -> exp t
+  ; op : edispatch -> exp t
   }
 
 let efun args rhs = List.fold_right efun args rhs
@@ -288,7 +295,12 @@ let pack =
       let case = lift2 ccase (tbar *> pat <* tarrow) (d.exp d) in
       lift2 ematch (tmatch *> d.exp d <* twith) (many1 case)
     in
-    ws *> choice [ elet; eif; efun; ematch ] <* ws
+    peek_char_fail
+    >>= function
+    | 'l' -> elet
+    | 'i' -> eif
+    | 'f' -> efun
+    | _ -> ematch
   in
   let tuple d =
     ws
@@ -307,38 +319,42 @@ let pack =
       ws *> choice [ lst >>| elist; uns_const >>| econst; id >>| evar; parens @@ d.exp d ]
       <* ws
     in
-    let app = chainr1 prim eapp in
+    let app = chainl1 prim eapp in
     let mul =
       ws
       *> choice
-           [ lift2 (eop Mul) (app <* token "*") (chainl1 (app <|> d.key d) mul); app ]
+           [ lift2 (eop Mul) (app <* token "*") (chainr1 (app <|> d.key d) mul); app ]
       <* ws
     in
-    let infop =
-      choice
-        [ token "-" *> mul >>| eunop Minus
-        ; token "+" *> mul
-        ; token "!" *> mul >>| eunop Not
-        ; mul
-        ]
-    in
     let add =
+      let infop p =
+        choice
+          [ token "-" *> p >>| eunop Minus
+          ; token "!" *> p >>| eunop Not
+          ; token "+" *> p
+          ; p
+          ]
+      in
       ws
       *> choice
-           [ lift2 (eop Add) (infop <* token "+") (chainl1 (infop <|> d.key d) add)
-           ; infop
+           [ lift2
+               (eop Add)
+               (infop mul <* token "+")
+               (chainr1 (infop mul <|> infop @@ d.key d) add)
+           ; infop mul
            ]
       <* ws
     in
     let cons =
       ws
-      *> choice [ lift2 econs (add <* token "::") (chainl1 (add <|> d.key d) cons); add ]
+      *> choice
+           [ lift2 econs (add <* token "::") (chainr1 (add <|> d.key d) eopcons); add ]
       <* ws
     in
     let cmp =
       ws
       *> choice
-           [ lift2 (eop Less) (cons <* token "<") (chainl1 (cons <|> d.key d) less)
+           [ lift2 (eop Less) (cons <* token "<") (chainr1 (cons <|> d.key d) less)
            ; cons
            ]
       <* ws
@@ -346,68 +362,201 @@ let pack =
     let and_ =
       ws
       *> choice
-           [ lift2 (eop And) (cmp <* token "&&") (chainl1 (cmp <|> d.key d) _and); cmp ]
+           [ lift2 (eop And) (cmp <* token "&&") (chainr1 (cmp <|> d.key d) _and); cmp ]
       <* ws
     in
     ws
     *> choice
-         [ lift2 (eop Or) (cmp <* token "||") (chainl1 (and_ <|> d.key d) _or); and_ ]
+         [ lift2 (eop Or) (cmp <* token "||") (chainr1 (and_ <|> d.key d) _or); and_ ]
     <* ws
   in
   { key; tuple; exp; op }
 ;;
 
 let exp = pack.exp pack
+
+(****************** Decl parsing ******************)
+
+let decl =
+  ws
+  *> lift3
+       dlet
+       (tlet *> option false (trec >>| fun _ -> true))
+       pat
+       (lift2 efun (ws *> many pat <* token "=") exp)
+  <* ws
+;;
+
+(****************** Decl parsing ******************)
+
+let pprog (l : decl list) : prog = l
+
+(****************** Prog parsing ******************)
+
+let prog = many1 (skip *> decl <* option "" (token ";;") <* skip) >>| pprog
+
+(****************** Tests ******************)
 let test_exp_suc = parse_test_suc pp_exp exp
+let test_pat_suc = parse_test_suc pp_pat pat
+let test_prog_suc = parse_test_suc pp_prog prog
 
 let%test _ =
-  test_exp_suc "8 :: x :: (match x with | _ -> 0) :: []"
-  @@ EVar ""
+  test_prog_suc
+    {|
+let fold init f lst = match lst with | [] -> acc | hd :: tl -> fold (f init hd) f tl
+|}
+  @@ [ DLet
+         ( false
+         , PVar "fold"
+         , EFun
+             ( PVar "init"
+             , EFun
+                 ( PVar "f"
+                 , EFun
+                     ( PVar "lst"
+                     , EMatch
+                         ( EVar "lst"
+                         , [ PList [], EVar "acc"
+                           ; ( PCons (PVar "hd", PVar "tl")
+                             , EApp
+                                 ( EApp
+                                     ( EApp
+                                         ( EVar "fold"
+                                         , EApp (EApp (EVar "f", EVar "init"), EVar "hd")
+                                         )
+                                     , EVar "f" )
+                                 , EVar "tl" ) )
+                           ] ) ) ) ) )
+     ]
+;;
 
 let%test _ =
-  test_exp_suc "let f x y = fun g z -> match d, x, (g z) with | _ -> true in false"
-  @@ ELet
-       ( [ ( false
-           , PVar "f"
-           , EFun
-               ( PVar "x"
-               , EFun
-                   ( PVar "y"
-                   , EFun
-                       ( PVar "g"
-                       , EFun
-                           ( PVar "z"
-                           , EMatch
-                               ( ETuple [ EVar "d"; EVar "x"; EApp (EVar "g", EVar "z") ]
-                               , [ PWild, EConst (CBool true) ] ) ) ) ) ) )
-         ]
-       , EConst (CBool false) )
+  test_prog_suc
+    {|
+let rec fact n = match n with | 0 -> 1 | _ -> n * fact (n + -1)
+|}
+    [ DLet
+        ( true
+        , PVar "fact"
+        , EFun
+            ( PVar "n"
+            , EMatch
+                ( EVar "n"
+                , [ PConst (CInt 0), EConst (CInt 1)
+                  ; ( PWild
+                    , EOp
+                        ( Mul
+                        , EVar "n"
+                        , EApp
+                            ( EVar "fact"
+                            , EOp (Add, EVar "n", EUnOp (Minus, EConst (CInt 1))) ) ) )
+                  ] ) ) )
+    ]
 ;;
 
-(* let%test _ =
-  test_exp_suc
-    "let rec map f l = match l with | [] -> [] | hd :: [tl] -> (f hd) :: (map f tl) in 1"
-  @@ ELet
-       ( [ ( true
-           , PVar "map"
-           , EFun
-               ( PVar "f"
-               , EFun
-                   ( PVar "l"
-                   , EMatch
-                       ( EVar "l"
-                       , [ PList [], EList []
-                         ; ( PList [ PVar "hd"; PVar "tl" ]
-                           , ECons
-                               ( EApp (EVar "f", EVar "hd")
-                               , EApp (EVar "map", EApp (EVar "f", EVar "tl")) ) )
-                         ] ) ) ) )
-         ]
-       , EConst (CInt 1) )
-;; *)
-
-let x =
-  match 10 with
-  | 5 -> 1
-  | _ -> 3
+let%test _ =
+  test_prog_suc
+    {|
+let a, b, (c, (d, e)) = 1, 2, (3, (4, 5));; let f _ = a + b + c + d + e;;
+|}
+    [ DLet
+        ( false
+        , PTuple
+            [ PVar "a"; PVar "b"; PTuple [ PVar "c"; PTuple [ PVar "d"; PVar "e" ] ] ]
+        , ETuple
+            [ EConst (CInt 1)
+            ; EConst (CInt 2)
+            ; ETuple [ EConst (CInt 3); ETuple [ EConst (CInt 4); EConst (CInt 5) ] ]
+            ] )
+    ; DLet
+        ( false
+        , PVar "f"
+        , EFun
+            ( PWild
+            , EOp
+                ( Add
+                , EVar "a"
+                , EOp (Add, EVar "b", EOp (Add, EVar "c", EOp (Add, EVar "d", EVar "e")))
+                ) ) )
+    ]
 ;;
+
+let%test _ =
+  test_prog_suc "let map f l = match l with | [] -> [] | hd :: tl -> f hd :: map f tl"
+  @@ [ DLet
+         ( false
+         , PVar "map"
+         , EFun
+             ( PVar "f"
+             , EFun
+                 ( PVar "l"
+                 , EMatch
+                     ( EVar "l"
+                     , [ PList [], EList []
+                       ; ( PCons (PVar "hd", PVar "tl")
+                         , ECons
+                             ( EApp (EVar "f", EVar "hd")
+                             , EApp (EApp (EVar "map", EVar "f"), EVar "tl") ) )
+                       ] ) ) ) )
+     ]
+;;
+
+let%test _ =
+  test_prog_suc "let x, y = 1 :: [2; 3], match z with | _ -> 4 :: 5 :: 6 :: rest;;"
+  @@ [ DLet
+         ( false
+         , PTuple [ PVar "x"; PVar "y" ]
+         , ETuple
+             [ ECons (EConst (CInt 1), EList [ EConst (CInt 2); EConst (CInt 3) ])
+             ; EMatch
+                 ( EVar "z"
+                 , [ ( PWild
+                     , ECons
+                         ( EConst (CInt 4)
+                         , ECons (EConst (CInt 5), ECons (EConst (CInt 6), EVar "rest"))
+                         ) )
+                   ] )
+             ] )
+     ]
+;;
+
+let%test _ =
+  test_prog_suc
+    "let fib n = let rec helper fst snd n = match n with | 0 -> fst | 1 -> snd | _ -> \
+     helper snd (fst + snd) (n + -1) in helper 0 1 n "
+  @@ [ DLet
+         ( false
+         , PVar "fib"
+         , EFun
+             ( PVar "n"
+             , ELet
+                 ( [ ( true
+                     , PVar "helper"
+                     , EFun
+                         ( PVar "fst"
+                         , EFun
+                             ( PVar "snd"
+                             , EFun
+                                 ( PVar "n"
+                                 , EMatch
+                                     ( EVar "n"
+                                     , [ PConst (CInt 0), EVar "fst"
+                                       ; PConst (CInt 1), EVar "snd"
+                                       ; ( PWild
+                                         , EApp
+                                             ( EApp
+                                                 ( EApp (EVar "helper", EVar "snd")
+                                                 , EOp (Add, EVar "fst", EVar "snd") )
+                                             , EOp
+                                                 ( Add
+                                                 , EVar "n"
+                                                 , EUnOp (Minus, EConst (CInt 1)) ) ) )
+                                       ] ) ) ) ) )
+                   ]
+                 , EApp
+                     ( EApp (EApp (EVar "helper", EConst (CInt 0)), EConst (CInt 1))
+                     , EVar "n" ) ) ) )
+     ]
+;;
+
+(* let%test _ = test_exp_suc "let f x y = x + y in f 1 2" @@ EVar "" *)

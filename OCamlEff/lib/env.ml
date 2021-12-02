46c7cb875d9ceee9337e69b91ec45825688f9382
zishkaz <loktevsergay.2003@gmail.com>
2021-12-02 08:07:34 +0300

    added context

--- OCamlEff/lib/env.ml
+++ OCamlEff/lib/env.ml
@@ -6,14 +6,29 @@ module IdMap = Map.Make (struct
   let compare = compare
 end)
 
-type 'a t = 'a IdMap.t
+type 'a id_t = 'a IdMap.t
+
+module EffMap = Map.Make (struct
+  type t = id
+
+  let compare = compare
+end)
+
+type 'a eff_t = 'a EffMap.t
 
 exception Not_bound
 
-let empty = IdMap.empty
-let extend id x = IdMap.add id x
+let empty_id_map = IdMap.empty
+let empty_eff_map = EffMap.empty
+let extend_id_map id x = IdMap.add id x
+let extend_eff_map id x = EffMap.add id x
 
-let lookup id env =
+let lookup_id_map id env =
   try IdMap.find id env with
+  | Not_found -> raise Not_bound
+;;
+
+let lookup_eff_map id context =
+  try EffMap.find id context with
   | Not_found -> raise Not_bound
 ;;

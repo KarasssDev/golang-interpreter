c12f97aef4cd13003f4b3cde67f355a8a7b207a3
KarasssDev <viktorkarasev27@gmail.com>
2022-04-24 22:45:39 +0300

    updated main (now be able to run go file)

--- Golang/app/Main.hs
+++ Golang/app/Main.hs
@@ -1,48 +1,26 @@
 module Main where
-import Ast
-import Interpreter
-import Control.Monad.State
+
+import Control.Monad.State.Lazy
 import Data.Map
 
 import Interpreter
 import Ast
 import Errors
-import Runtime
+import Runtime hiding (head)
+import Parser
+import System.Environment
 
+main = do
+  args     <- getArgs
+  let fileName = head args
+  inp      <- readFile fileName
+  let prog = cleanFile inp
+  case (goParse prog) of
+    Left err -> print err
+    Right p  -> do
+      exec p
+      return ()
 
-
-e :: GoExpr 
-e = Minus (Val (VInt 2)) (Var "x")
-
-b :: GoStatement 
-b = Block [VarDecl "x" TInt (Val (VInt 3)), 
-           VarDecl "y" TInt (Minus (Val (VInt 2)) (Var "x")),
-           Print (Var "y")
-           ]
-
-
-
-pprint :: [String] -> IO ()
-pprint [] = undefined
-pprint [x] = print x
-pprint (x:xs) = do
-    print x 
-    pprint xs 
-
-showVars :: GoStatement -> IO () 
-showVars s = do
-    let v = vars $ execState (evalStatement s) emptyGoRuntime
-    pprint $ keys v
-
-showVals :: GoStatement -> IO () 
-showVals s = do
-    let v = vars $ execState (evalStatement s) emptyGoRuntime
-    pprint $ Prelude.map show (elems v)
-
-exec :: GoStatement -> IO ()
-exec s = do
-    let r = execState (evalStatement s) emptyGoRuntime
-    pprint $ toPrint r 
-
-main :: IO ()
-main = exec b
+-- main = do
+--   s <- getLine
+--   p s

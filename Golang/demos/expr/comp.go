d2622bb86ad92c0bcb52f3bb04d11f3468bc5c6f
KarasssDev <viktorkarasev27@gmail.com>
2022-04-25 13:23:57 +0300

    updated tests

---
+++ Golang/demos/expr/comp.go
@@ -0,0 +1,11 @@
+package main;
+
+func main() {
+	println(0 < 1);
+	println(0 <= 1);
+	println(0 == 0);
+	println(0 != 0);
+	println(0 != 1 );
+	println(1 + 1 == 2);
+	println(1 + 1 == 3 - 1);
+}
--- Golang/demos/func/fib.go
+++ Golang/demos/func/fib.go
@@ -1,4 +1,4 @@
-package main
+package main;
 
 func fib(n int) int {
 	if (n == 0) {

158b4170c4b157b66644609a92c5af6daa5fa990
KarasssDev <viktorkarasev27@gmail.com>
2022-05-12 20:33:24 +0300

    added handling error(missed file name)

--- app/Main.hs
+++ app/Main.hs
@@ -8,14 +8,15 @@ import System.Environment ( getArgs )
 main :: IO ()
 main = do
   args     <- getArgs
-  let fileName = head args
-  inp      <- readFile fileName
-  let prog = cleanFile inp
-  case goParse prog of
-    Left err -> print err
-    Right p  -> do
-      (res, _) <- exec p
-      case res of 
-        (Left err) -> print err
-        (Right _)  -> return ()
-
+  case args of 
+    fileName:_ -> do
+      inp      <- readFile fileName
+      let prog = cleanFile inp
+      case goParse prog of
+        Left err -> print err
+        Right p  -> do
+          (res, _) <- exec p
+          case res of 
+            (Left err) -> print err
+            (Right _)  -> return ()
+    [] -> print "Expected filename in args"
